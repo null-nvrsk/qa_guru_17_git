daa7704bdacc7eef0bc7bdaed9f15c2a79aa3665
Maksim Skomorokhov <53369027+null-nvrsk@users.noreply.github.com>
2024-03-11 17:26:08 +0300

    add output in new test

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class NewTest {
     @Test
     void newTest() {
+        System.out.println("123");
         assertTrue(3 > 2);
     }
 }

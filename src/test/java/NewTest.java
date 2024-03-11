4686d355abde239b9cc086503b12307154e2e586
Maksim Skomorokhov <53369027+null-nvrsk@users.noreply.github.com>
2024-03-11 21:13:34 +0300

    edit var i

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class NewTest {
     @Test
     void newTest() {
-        int i = 3;
+        int i = 10;
         System.out.println("123");
         assertTrue(i > 2);
     }
